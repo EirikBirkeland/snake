392e0a07a1fc00ee596fec5e99873a45a82dd86b
Eirik Birkeland <birketrans@gmail.com>
2018-09-23 21:09:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# snake
-An attempt at recreating the classic Snake
+# Pong
 
-https://codepen.io/eirikbirkeland/pen/pOBePz?editors=1010
+This is my first take on the classic Pong.
+I enthusiastically built this in order to get an idea of what it means to actually make a video game from scratch using HTML5 canvas, without any game libraries.
+
+The primary challenge was implementing proper collision detection.
+
+This game is really easy to re-create. I recommend anyone to implement this from scratch as their first video game. Using basic concepts, you should be able to implement Pong in no time!
+
+Codepen.io version:
+https://codepen.io/eirikbirkeland/pen/BPMPrp
+
+## Gameplay
+![gameplay animation](gameplay_anim.gif)
+Notice the subtle and intriguing bugs stemming from poor collision detection.
---
+++ index.js
@@ -0,0 +1,146 @@
+import { getRandomInt } from './lib/util';
+import ScoreBoard from './lib/ScoreBoard';
+import Snake from './lib/Snake';
+import Square from './lib/Square';
+
+const GRID_X_LENGTH = 20;
+const GRID_Y_LENGTH = 20;
+
+const keys = {
+    w: { isPressed: false },
+    s: { isPressed: false },
+    a: { isPressed: false },
+    d: { isPressed: false }
+};
+
+window.addEventListener("keydown", e => {
+    e.preventDefault();
+    keys.w.isPressed = e.which === 87;
+    keys.s.isPressed = e.which === 83;
+});
+
+window.addEventListener("keyup", e => {
+    e.preventDefault();
+    keys.w.isPressed = e.which === 87;
+    keys.s.isPressed = e.which === 83;
+});
+
+window.addEventListener("keydown", e => {
+    e.preventDefault();
+    keys.a.isPressed = e.which === 65;
+    keys.d.isPressed = e.which === 68;
+});
+
+window.addEventListener("keyup", e => {
+    e.preventDefault();
+    keys.a.isPressed = e.which === 65;
+    keys.d.isPressed = e.which === 68;
+});
+
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+
+const grid = Array(GRID_X_LENGTH)
+    .fill(true)
+    .map((_, i) => {
+        return Array(GRID_Y_LENGTH)
+            .fill(true)
+            .map((_, j) => new Square(i, j));
+    });
+
+let snake = new Snake(GRID_X_LENGTH / 2, GRID_Y_LENGTH / 2);
+let apple = new Apple(3, 5);
+
+const scoreBoard = new ScoreBoard();
+
+let timer = 0;
+
+function loop() {
+    grid.forEach(a => {
+        a.forEach(b => {
+            b.render();
+        });
+    });
+
+    // check for collisions
+    if (apple && snake.X() === apple.X && snake.Y() === apple.Y) {
+        // eat apple
+        snake.justAte = true;
+        scoreBoard.increment();
+        apple = new Apple(getRandomInt(1, 19), getRandomInt(1, 19));
+    }
+
+    // check whether user is pushing keys and change snake coords accordingly
+    if (keys.w.isPressed) {
+        // unless snake tries to escape board
+        if (!(snake.Y() - 1 < 0) && snake.direction !== "down") {
+            snake.moveDebounced("up");
+            keys.w.isPressed = false;
+        }
+    } else if (keys.s.isPressed && snake.direction !== "up") {
+        if (!(snake.Y() + 1 > GRID_Y_LENGTH - 1)) {
+            snake.moveDebounced("down");
+            keys.s.isPressed = false;
+        }
+    }
+    if (keys.a.isPressed && snake.direction !== "right") {
+        if (!(snake.X() - 1 < 0)) {
+            snake.moveDebounced("left");
+            keys.a.isPressed = false;
+        }
+    } else if (keys.d.isPressed && snake.direction !== "left") {
+        if (!(snake.X() + 1 > GRID_X_LENGTH - 1)) {
+            snake.moveDebounced("right");
+            keys.d.isPressed = false;
+        }
+    }
+
+    // check collision with self
+    const headSegment = snake.segments[0];
+    const bodySegments = snake.segments.slice(1);
+
+    bodySegments.forEach((bodySeg, index) => {
+        if (headSegment.X === bodySeg.X && headSegment.Y === bodySeg.Y) {
+            snake = new Snake(GRID_X_LENGTH / 2, GRID_Y_LENGTH / 2);
+            scoreBoard.reset();
+        }
+    });
+
+    // move snake in current direction
+    if (timer >= 60 - snake.segments.length * 2) {
+        timer = 0;
+        switch (snake.direction) {
+            case "up":
+                if (!(snake.Y() - 1 < 0)) {
+                    snake.move("up");
+                }
+                break;
+            case "down":
+                if (!(snake.Y() > GRID_Y_LENGTH - 2)) {
+                    snake.move("down");
+                }
+                break;
+            case "left":
+                if (!(snake.X() - 1 < 0)) {
+                    snake.move("left");
+                }
+                break;
+            case "right":
+                if (!(snake.X() + 1 > GRID_X_LENGTH - 1)) {
+                    snake.move("right");
+                }
+                break;
+        }
+    }
+
+    if (timer < 60)++timer;
+
+    // render sprites
+    snake.render();
+    apple.render();
+    scoreBoard.render();
+
+    return window.requestAnimationFrame(loop);
+}
+
+loop();
